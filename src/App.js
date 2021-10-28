8f73794ba6f1677049d3e2096656bd89674a5a25
JwhCK <jacobwoord@gmail.com>
2021-10-28 14:41:01 +0200

    scroll component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import { Component } from "react";
 import './App.css'
+import Scroll from './Scroll'
 
 
 
@@ -11,11 +11,17 @@ class App extends Component{
     constructor(){
         super()
         this.state={
-            robots: robots,
+            robots: [],
             searchfield: ''
 
         }
     }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))}
+        
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     
@@ -27,9 +33,11 @@ class App extends Component{
         })
         return(
             <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
+            <h1 className='f1zz'>RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll>
             <CardList robots = {filteredRobots}/>
+            </Scroll>
             </div>
            
         );
