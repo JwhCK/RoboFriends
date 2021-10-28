467379c76f186fbfb92e91b4b4359fb1fff20b99
JwhCK <jacobwoord@gmail.com>
2021-10-28 11:56:52 +0200

    neew commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import App from './App'
+
+
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <div>
+      <App/>
+   </div>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jacob Woord',
+        username:'eatAPeach',
+        email:'Jacobwoord@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Maria Woord',
+        username:'AppleJuice',
+        email:'marianentjes@hotmail.com'
+    },
+    {
+        id: 3,
+        name: 'sjoerd vd berg',
+        username:'proxima',
+        email:'sjoerd@123.nl'
+    },
+    {
+        id: 4,
+        name: 'siriusB',
+        username:'GLobal',
+        email:'globalus@hotmail.com'
+    },
+    {
+        id: 5,
+        name: 'siriusB',
+        username:'GLobal',
+        email:'globalus@hotmail.com'
+    },
+    
+];
